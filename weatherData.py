289a72a3018a6e4faca27438ab36c111c5374646
Jerfenson Cerda <cerdamejiaj@allegheny.edu>
2018-11-01 14:18:13 -0400

    Trying to scrape data from a stock database. Still need to figure out where to get the data from the chart.

--- weatherData.py
+++ weatherData.py
@@ -5,8 +5,6 @@ import imgkit
 
 
 
-
-
 page = requests.get("http://forecast.weather.gov/MapClick.php?lat=37.7772&lon=-122.4168")
 soup = BeautifulSoup(page.content, 'html.parser')
 seven_day = soup.find(id="seven-day-forecast")
