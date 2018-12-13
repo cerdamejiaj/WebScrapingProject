5102eb23742df190a7741e1944b7639639222eb9
Jerfenson Cerda <cerdamejiaj@allegheny.edu>
2018-12-13 16:07:59 -0500

    need to remove tkinter from the travis build because it can't be testing on that enviroment sincie it a gui displat.

--- src/mainForTravisBuild.py
+++ src/mainForTravisBuild.py
@@ -16,15 +16,15 @@ def main():
 if __name__ == '__main__':
 
     main()
-    root = tkinter.Tk()
-    with open("../build/weather.csv", newline = "") as file:
-        reader = csv.reader(file)
-        r = 2
-        for col in reader:
-            c = 2
-            for row in col:
-                label = tkinter.Label(root, width = 25, height = 3, text = row, relief = tkinter.RIDGE)
-                label.grid(row = r, column = c)
-                c += 1
-            r += 1
-    root.mainloop()
+    # root = tkinter.Tk()
+    # with open("../build/weather.csv", newline = "") as file:
+    #     reader = csv.reader(file)
+    #     r = 2
+    #     for col in reader:
+    #         c = 2
+    #         for row in col:
+    #             label = tkinter.Label(root, width = 25, height = 3, text = row, relief = tkinter.RIDGE)
+    #             label.grid(row = r, column = c)
+    #             c += 1
+    #         r += 1
+    # root.mainloop()
