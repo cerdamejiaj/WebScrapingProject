d6034dc275e2d20e146245740f617ea2ceb70208
Jerfenson Cerda <cerdamejiaj@allegheny.edu>
2018-12-13 16:41:40 -0500

    testing if tkinter will workd with build

--- src/mainForTravisBuild.py
+++ src/mainForTravisBuild.py
@@ -1,6 +1,8 @@
 import weatherDataForTravisBuild
 import tkinter
 import csv
+import matplotlib
+
 
 
 def main():
@@ -16,15 +18,16 @@ def main():
 if __name__ == '__main__':
 
     main()
-    # root = tkinter.Tk()
-    # with open("../build/weather.csv", newline = "") as file:
-    #     reader = csv.reader(file)
-    #     r = 2
-    #     for col in reader:
-    #         c = 2
-    #         for row in col:
-    #             label = tkinter.Label(root, width = 25, height = 3, text = row, relief = tkinter.RIDGE)
-    #             label.grid(row = r, column = c)
-    #             c += 1
-    #         r += 1
-    # root.mainloop()
+    matplotlib.use('Agg')
+    root = tkinter.Tk()
+    with open("../build/weather.csv", newline = "") as file:
+        reader = csv.reader(file)
+        r = 2
+        for col in reader:
+            c = 2
+            for row in col:
+                label = tkinter.Label(root, width = 25, height = 3, text = row, relief = tkinter.RIDGE)
+                label.grid(row = r, column = c)
+                c += 1
+            r += 1
+    root.mainloop()
