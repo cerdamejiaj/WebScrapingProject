d9a772d4fb6a3fa335a757f1a463d5b8d773f295
jerfenson <cerdamejiaj@allegheny.edu>
2018-12-13 18:10:47 -0500

    still counld not get tkinter to work in travis ci

--- src/mainForTravisBuild.py
+++ src/mainForTravisBuild.py
@@ -18,15 +18,15 @@ def main():
 if __name__ == '__main__':
     matplotlib.use('Agg')
     main()
-    root = tkinter.Tk()
-    with open("../build/weather.csv", newline = "") as file:
-        reader = csv.reader(file)
-        r = 2
-        for col in reader:
-            c = 2
-            for row in col:
-                label = tkinter.Label(root, width = 25, height = 3, text = row, relief = tkinter.RIDGE)
-                label.grid(row = r, column = c)
-                c += 1
-            r += 1
-    root.mainloop()
+    # root = tkinter.Tk()
+    # with open("../build/weather.csv", newline = "") as file:
+    #     reader = csv.reader(file)
+    #     r = 1
+    #     for col in reader:
+    #         c = 1
+    #         for row in col:
+    #             label = tkinter.Label(root, width = 25, height = 3, text = row, relief = tkinter.RIDGE)
+    #             label.grid(row = r, column = c)
+    #             c += 1
+    #         r += 1
+    # root.mainloop()
